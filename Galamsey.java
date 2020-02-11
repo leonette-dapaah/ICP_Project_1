9fcd51a58e29f1a79e91f56fc42275f74498235c
leonette-dapaah <60904579+leonette-dapaah@users.noreply.github.com>
2020-02-11 13:20:00 +0000

    Created the  Galamsey.java and MonitoringIO.java

---
+++ Galamsey.java
@@ -0,0 +1,78 @@
+public class Galamsey {
+
+    String veg_colour;
+    String position;
+    int colour_val;
+    int year;
+
+    public Galamsey(){
+
+    }
+
+    /**
+     * An overloaded Galamsey constructor
+     * */
+    public Galamsey(String veg_colour, String position,
+                    int colour_val, int year){
+        this.veg_colour = veg_colour;
+        this.position = position;
+        this.colour_val = colour_val;
+        this.year = year;
+    }
+
+    /**
+     * @return Vegetation Colour
+     * */
+    public String getVeg_colour() {
+        return veg_colour;
+    }
+
+    /**
+     * @param veg_colour
+     * */
+    public void setVeg_colour(String veg_colour) {
+        this.veg_colour = veg_colour;
+    }
+
+    /**
+     * @return Vegetation Colour
+     * */
+    public String getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position
+     * */
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    /**
+     * @return Colour Value
+     * */
+    public int getColour_val() {
+        return colour_val;
+    }
+
+    /**
+     * @param colour_val
+     * */
+    public void setColour_val(int colour_val) {
+        this.colour_val = colour_val;
+    }
+
+    /**
+     * @return Year of the event
+     * */
+    public int getYear() {
+        return year;
+    }
+
+    /**
+     * @param year
+     * */
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ MonitoringIO.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class MonitoringIO {
+    public static void main (String[] args){
+        Scanner s = new Scanner(System.in);
+        int u_choice = s.nextInt();
+        System.out.println("          MENU        ");
+        System.out.println("Enter 1 for observatory data");
+        System.out.println("Enter 2 for 'galamsey' data");
+        System.out.println("Enter 3 for Monitoring statistics");
+        System.out.println("Enter 4 to Exit");
+
+        switch (u_choice){
+            case 1:
+                System.out.println("Name of observatory: ");
+                String name = s.next();
+
+                System.out.println("Enter country location: ");
+                String location = s.next();
+
+                System.out.println("Enter year galamsey operations began: ");
+                int yr = s.nextInt();
+
+                System.out.println("Enter area covered by the observatory (in square kilometers): ");
+                double col = s.nextDouble();
+
+                System.out.println("List of 'galamsey' events recorded: ");
+                System.out.println();
+
+                break;
+
+            case 2:
+                System.out.println("Enter vegetation colour: ");
+                String colour = s.next();
+
+                System.out.println("Enter position (Latitude and Longitude: ): ");
+                String position = s.next();
+
+                System.out.println("Enter colour value: ");
+                int col_val = s.nextInt();
+
+                System.out.println("Enter year of event: ");
+                int year = s.nextInt();
+                break;
+
+            case 3:
+                System.out.println();
+                break;
+
+            default:
+                System.out.println("Invalid Input");
+
+            case 4:
+                System.exit(0);
+        }
+    }
+}
