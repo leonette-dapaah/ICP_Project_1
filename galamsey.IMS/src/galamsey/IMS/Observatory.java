8325a2e604ac033759d6be1f771e4a92df518ca4
Abdul-Fuseni <abdul.fuseini@ashesi.edu.gh>
2020-02-11 14:59:06 +0000

    adding the observatory class

---
+++ galamsey.IMS/src/galamsey/IMS/Observatory.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+/**
+ * @author Abdul-Aziz
+ *
+ */
+public class Observatory {
+	
+	private String obsName;
+	private String country;
+	private int yearStarted;
+	private double areaCovered;
+	private ArrayList<String>list;
+	
+	public Observatory(String obsName, String country, int yearStarted, double areaCovered) {
+		this.obsName = obsName;
+		this.country = country;
+		this.yearStarted = yearStarted;
+	}
+
+	/**
+	 * @return the obsName
+	 */
+	public String getObsName() {
+		return obsName;
+	}
+
+	/**
+	 * @param obsName the obsName to set
+	 */
+	public void setObsName(String obsName) {
+		this.obsName = obsName;
+	}
+
+	/**
+	 * @return the country
+	 */
+	public String getCountry() {
+		return country;
+	}
+
+	/**
+	 * @param country the country to set
+	 */
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	/**
+	 * @return the yearStarted
+	 */
+	public int getYearStarted() {
+		return yearStarted;
+	}
+
+	/**
+	 * @param yearStarted the yearStarted to set
+	 */
+	public void setYearStarted(int yearStarted) {
+		this.yearStarted = yearStarted;
+	}
+
+	/**
+	 * @return the areaCovered
+	 */
+	public double getAreaCovered() {
+		return areaCovered;
+	}
+
+	/**
+	 * @param areaCovered the areaCovered to set
+	 */
+	public void setAreaCovered(double areaCovered) {
+		this.areaCovered = areaCovered;
+	}
+	
+
+}
