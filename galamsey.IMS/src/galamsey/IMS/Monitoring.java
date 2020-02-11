e001cf8c65ddf26f29132160c17346a4b9ca2505
hussein6065 <fuseinial@gmail.com>
2020-02-11 11:57:35 +0000

    Created the Monitoring class

---
+++ galamsey.IMS/src/galamsey/IMS/Monitoring.java
@@ -0,0 +1,29 @@
+package galamsey.IMS;
+import java.util.ArrayList;
+
+public class Monitoring {
+	/**
+	 * @author Hussein Fuseini
+	 * @version 1.0.1
+	 */
+	
+	
+	private ArrayList<String> Observatories;
+	
+	public Monitoring() {
+		
+	}
+	// This method returns the largest “galamsey” colour value ever recorded.
+	public int largestGalamColVal() {
+		return 0;
+	}
+	// This method returns A list of all “galamsey” recorded with colour 
+	// value greater than a given/arbitrary number.
+	public ArrayList<String> getAllRecordsAbove(int value){
+		return new ArrayList<String>();
+	}
+	// This method returns The observatory with the largest average “galamsey” colour value.
+	public String getObservatoryWithHighAverage() {
+		return "Hussein";
+	}
+}
